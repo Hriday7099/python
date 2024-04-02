0046b6f5a9bdabd2e6f2bb22eca709466f643a5f
Hriday7099 <128073494+Hriday7099@users.noreply.github.com>
2024-04-02 10:32:48 +0530

    Add files via upload

---
+++ python/EXP4.1.py
@@ -0,0 +1,8 @@
+#WAP to show working of a while Loop with a text
+print("Name: Hriday\nRoll No: 2210997099")
+
+text = "I am Coding with Python"
+i = 0
+while i < len(text):
+    print(text[i])
+    i += 1
---
+++ python/EXP4.14.py
@@ -0,0 +1,5 @@
+# display 1 to 10 in reverse order
+print("Name: Hriday\nRoll No: 2210997099")
+
+for i in range(10, 0, -1):
+    print(i)
